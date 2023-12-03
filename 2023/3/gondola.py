a947ed7035f13431cdc0117334af3982d56d5bde
Jeff Schoner <jeffschoner@gmail.com>
2023-12-03 13:18:20 -0800

    Refactor find for Day 3, Part 2

--- 2023/3/gondola.py
+++ 2023/3/gondola.py
@@ -1,15 +1,20 @@
 import re
 
-def find_all_symbols(lines):
+def find_all(lines):
     symbols = set()
+    numbers = set()
     for row in range(len(lines)):
         line = lines[row]
+        for m in re.finditer("\d+", line):
+            start, end = m.span()
+            numbers.add((row, start, end, int(m.group(0))))
+
         for col in range(len(line)):
             o = ord(line[col])
             if o != 46 and (o < 48 or o > 57):
                 symbols.add((row, col))
 
-    return symbols
+    return symbols, numbers
 
 def has_adjacent_symbol(symbols, row, start_col, end_col):
     for r in [row-1, row+1]:
@@ -20,14 +25,11 @@ def has_adjacent_symbol(symbols, row, start_col, end_col):
 
 def run(text):
     lines = [line for line in text.split('\n') if line]
-    symbols = find_all_symbols(lines)
+    symbols, numbers = find_all(lines)
 
     sum = 0
-    for row in range(len(lines)):
-        line = lines[row]
-        for m in re.finditer("\d+", line):
-            start, end = m.span()
-            if has_adjacent_symbol(symbols, row, start, end):
-                sum += int(m.group(0))
+    for row, start_col, end_col, number in numbers:
+        if has_adjacent_symbol(symbols, row, start_col, end_col):
+            sum += number
 
     return sum
